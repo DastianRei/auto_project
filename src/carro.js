2552d758fad82cd7629aba643d3ded3913325f10
Mateo <mateo>
2023-04-05 15:02:11 -0400

    creamos la funcion para generar una matriz vacia

--- src/carro.js
+++ src/carro.js
@@ -1,7 +1,21 @@
-class Auto {
+class Auto
+{
   constructor(x, y,direccion) {
     this.x = x;
     this.y = y;
     this.direccion=direccion;
   }
+
+  generarTablero(filas, columnas)
+  {
+    let matriz = new Array(filas);
+  for (let i = 0; i < filas; i++) 
+  {
+    matriz[i] = new Array(columnas);
+  }
+
+  return matriz;
+  }
 }
+
+
