a13da94e792bd81fb05f53af7ff8c26f65910f2a
Dayan <dayan.aguilar@ucb.edu.bo>
2023-04-09 11:14:32 -0400

    creamos el label para que se muestre la posicion final y vemos que funciona de manera correcta

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,6 @@
 import Auto from "./carro";
 
-const div=document.querySelector("#divPosInicial");
+const labelPosFinal=document.querySelector("#posicion-final");
 const form=document.querySelector("#formulario");
 const posInicial=document.querySelector("#posicion-inicial")
 const comandos=document.querySelector("#comandos-input")
@@ -8,6 +8,7 @@ const posFinal=document.querySelector("#posicion-final")
 let auto=new Auto(0,0,"N");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  //ceceecce
-  div.innerHTML = "<p>" + auto.ejecutarInstrucciones("5,5/3,3E/AADAADADDA") + "</p>";
+  let cadena = comandos.value.split('/');
+  console.log(cadena);
+  labelPosFinal.innerHTML = "<p>" + auto.ejecutarInstrucciones(comandos.value) + "</p>";
 });
