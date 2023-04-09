c43305ada123eb9c1698004c392a0b2da2eb3c31
Dayan <dayan.aguilar@ucb.edu.bo>
2023-04-09 11:22:51 -0400

    creamos el label para que se muestren los comandos

--- src/presenter.js
+++ src/presenter.js
@@ -2,16 +2,16 @@ import Auto from "./carro";
 
 const labelPosFinal=document.querySelector("#posicion-final");
 const labelPosInicial=document.querySelector("#posicion-inicial");
+const labelComandos=document.querySelector("#comandos");
 const form=document.querySelector("#formulario");
-const posInicial=document.querySelector("#posicion-inicial")
 const comandos=document.querySelector("#comandos-input")
-const posFinal=document.querySelector("#posicion-final")
 let auto=new Auto(0,0,"N");
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   let cadena = comandos.value.split('/');
   console.log(cadena);
   labelPosInicial.innerHTML = cadena[1];
-
+  labelComandos.innerHTML = cadena[2];
   labelPosFinal.innerHTML =  auto.ejecutarInstrucciones(comandos.value) ;
 });
