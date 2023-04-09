2140327c3091d4bc482b6af4cdb05a11dcfab759
Dayan <dayan.aguilar@ucb.edu.bo>
2023-04-09 11:18:33 -0400

    creamos el label para que se muestre la posicion inicial

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,7 @@
 import Auto from "./carro";
 
 const labelPosFinal=document.querySelector("#posicion-final");
+const labelPosInicial=document.querySelector("#posicion-inicial");
 const form=document.querySelector("#formulario");
 const posInicial=document.querySelector("#posicion-inicial")
 const comandos=document.querySelector("#comandos-input")
@@ -10,5 +11,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   let cadena = comandos.value.split('/');
   console.log(cadena);
-  labelPosFinal.innerHTML = "<p>" + auto.ejecutarInstrucciones(comandos.value) + "</p>";
+  labelPosInicial.innerHTML = cadena[1];
+
+  labelPosFinal.innerHTML =  auto.ejecutarInstrucciones(comandos.value) ;
 });
